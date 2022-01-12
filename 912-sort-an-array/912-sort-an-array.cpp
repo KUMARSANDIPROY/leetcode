e50eded0b06d13b9c9ff7f7057740e9b4445abf8
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-01-12 23:22:40 +0530

    Time: 184 ms (21.70%), Space: 28.7 MB (46.05%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -5,31 +5,26 @@ public:
         int mid=i+(j-i)/2;
         int l1=mid-i+1;
         int l2=j-mid;
-        int *first=new int[l1];
-        int *second=new int[l2];
-        
+        int left[l1],right[l2];
         int index=i;
-        
-        for(int p=0;p<l1;p++)
-            first[p]=nums[index++];
-        for(int p=0;p<l2;p++)
-            second[p]=nums[index++];
-        int p=0,q=0;
-        index=i;
-        while(p<l1 && q<l2)
+        for(int k=0;k<l1;k++)
+            left[k]=nums[index++];
+        for(int k=0;k<l2;k++)
+            right[k]=nums[index++];
+        int p1=0,q1=0,k1=i;
+         //cout<<"mbc";
+        while(p1<l1 && q1<l2)
         {
-            if(first[p]<second[q])
-            {
-                nums[index++]=first[p++];
-                
-            }
+            if(left[p1]<right[q1])
+                nums[k1++]=left[p1++];
             else
-                nums[index++]=second[q++];
+                nums[k1++]=right[q1++];
         }
-        while(p!=l1)
-            nums[index++]=first[p++];
-        while(q!=l2)
-            nums[index++]=second[q++];
+      // 
+        while(p1!=l1)
+            nums[k1++]=left[p1++];
+        while(q1!=l2)
+            nums[k1++]=right[q1++];
         
     }
                
