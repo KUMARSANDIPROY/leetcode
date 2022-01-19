6663a1d03ba72483ade093b0ca3fba1b6cf72dc8
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-01-20 01:37:12 +0530

    Time: 28 ms (23.99%), Space: 14.4 MB (28.04%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,31 +9,22 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode *root,TreeNode *k,vector<TreeNode*> &v)
-    {
-
-        if(root==NULL) return false;
-        v.push_back(root);
-        if(root==k) return true;
-        if(solve(root->left,k,v) || solve(root->right,k,v))
-            return true;
-        v.pop_back();
-        return false;
-    }
-        
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> v1,v2;
-        if(!root) return NULL;
-        solve(root,p,v1);
-        solve(root,q,v2);
-        int l=min(v1.size(),v2.size()),i=0;
-        while(i<l)
-        {
-           if(v1[i]!=v2[i])
-               break;
-            i++;
-        }
-        return v1[i-1];
+        
+       
+        if(root==NULL || root==p || root==q) return root;
+       
+        TreeNode *l=(lowestCommonAncestor(root->left,p,q));
+        TreeNode *r=(lowestCommonAncestor(root->right,p,q));
+        
+        if(l==NULL && r==NULL) return NULL;
+        if(l!=NULL && r==NULL) return l;
+        if(l==NULL && r!=NULL) return r;
+        else return root;//when both child return some value other than NULL             
+                     
+     
+        
+        
         
         
     }
