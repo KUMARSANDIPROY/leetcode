344a087359890dc40cc801e6cf288100d905af76
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-01-22 01:47:44 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (95.99%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void solve(string digits,string mapping[],vector<string> &ans,string output,int index)
+    {
+        if(index>=digits.length())
+        {
+            ans.push_back(output);
+            return;
+        }
+        int number=digits[index]-'0';
+        string s=mapping[number];
+        for(int i=0;i<s.length();i++)
+        {
+            output+=s[i];
+            solve(digits,mapping,ans,output,index+1);
+            output.pop_back();
+        }
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        string output="";
+        int index=0;
+        if(digits.length()==0)
+          return ans;
+        string mapping[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits,mapping,ans,output,index);
+        return ans;
+        
+    }
+};
