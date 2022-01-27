25f230b8845c078af30f5e5d78c887f7051a19cd
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-01-27 20:32:12 +0530

    Time: 15 ms (13.69%), Space: 12.8 MB (39.82%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -11,33 +11,27 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root,vector<TreeNode*> &v)
-    {
-        if(root==NULL) return;
-        
-        v.push_back(root);
-        preorder(root->left,v);
-        preorder(root->right,v);
-    }
-    
+   
     void flatten(TreeNode* root) {
-        vector<TreeNode*> v;
-        if(root==NULL) return;
-        
-        preorder(root,v);
-        
-        TreeNode *prev=root,*cur;
-        int n=v.size();
-        v.pop_back();
-        for(int i=1;i<n;i++)
-        {
-            cur=v[i];
-            prev->right=cur;
-            prev->left=NULL;
-            prev=cur;
-                
-        }
-        
+       TreeNode*now = root;
+		while (now)
+		{
+			if(now->left)
+			{
+                //Find current node's prenode that links to current node's right subtree
+				TreeNode* pre = now->left;
+				while(pre->right)
+				{
+					pre = pre->right;
+				}
+				pre->right = now->right;
+                //Use current node's left subtree to replace its right subtree(original right 
+                //subtree is already linked by current node's prenode
+				now->right = now->left;
+				now->left = NULL;
+			}
+			now = now->right;
+		}
         
     }
 };
