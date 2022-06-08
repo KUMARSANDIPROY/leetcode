d305275521b04809de0651ca5df03932901bab50
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-06-08 15:57:40 +0530

    Time: 123 ms (42.48%), Space: 14.4 MB (20.58%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,14 +3,16 @@ public:
     int solve(int r,int c1,int c2,vector<vector<int>> &grid,vector<vector<vector<int>>> &dp,int row,int col)
     {
       
+       
         if(c1<0 || c1>=col || c2<0 || c2>=col) return -1e9;
+  
         if(r==row-1)
         {
             if(c1==c2) return grid[r][c1];
             else return grid[r][c1]+ grid[r][c2];
         }
+                if(dp[r][c1][c2]!=-1) return dp[r][c1][c2];
         
-         if(dp[r][c1][c2]!=-1) return dp[r][c1][c2];
         int maxi=0;
         for(int i=-1;i<=1;i++)
         {
