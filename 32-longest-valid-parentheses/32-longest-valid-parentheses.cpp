978bef28f7dee56fac6c9d9c363f0ea47ee92ad7
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-06-11 00:03:47 +0530

    Time: 3 ms (89.90%), Space: 7.6 MB (10.73%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        stack<char> charst;
+        stack<int> indexst;
+        indexst.push(-1);
+        int len=0;
+        if(s.length()<=1) return 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+            {
+                charst.push('(');
+                indexst.push(i);
+            }
+            else if(s[i]==')')
+            {
+                if(!charst.empty() && charst.top()=='(')
+                {
+                    charst.pop();
+                    indexst.pop();
+                    len=max(len,i-indexst.top());
+                }
+                else if(charst.empty())
+                {
+                    indexst.push(i);
+                }
+            }
+        }
+        return len;
+    }
+};
