4fc5f765ae720d0f9d674d1ae90dde55cbd53c13
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-06-11 12:26:19 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -7,33 +7,30 @@ class Solution {
 	public:
 		int LongestRepeatingSubsequence(string str){
 		    // Code here
-		    
-        		int l=str.length(); 
-        		
-        		int t[l+1][l+1];
-        		for(int i=0;i<=l;i++)
-        		{
-        		    for(int j=0;j<=l;j++)
-        		    {
-        		        if(i==0 || j==0)
-        		          t[i][j]=0;
-        		    }
-        		}
-		        for(int i=1;i<=l;i++)
+		    int l=str.length();
+		   // vector<vector<int>> dp(l+1,vector<int>(l+1));
+		    int dp[l+1][l+1];
+		    int i,j;
+		    for( i=0;i<=l;i++)
+		    {
+		        for( j=0;j<=l;j++)
 		        {
-		            for(int j=1;j<=l;j++)
-		            {
-		                if(str[i-1]==str[j-1] && i!=j)
-		                {
-		                    t[i][j]=1+t[i-1][j-1];
-		                }
-		                else
-		                {
-		                    t[i][j]=max(t[i-1][j],t[i][j-1]);
-		                }
-		            }
+		            if(i==0 || j==0)
+		               dp[i][j]=0;
 		        }
-		        return t[l][l];
+		    }
+		    for(int i=1;i<=l;i++)
+		    {
+		        for(int j=1;j<=l;j++)
+		        {
+		            if(str[i-1]==str[j-1] && i!=j)
+		               dp[i][j]=1+dp[i-1][j-1];
+		            
+		            else
+		               dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		        }
+		    }
+		    return dp[l][l];
 		}
 
 };
