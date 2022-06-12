885a98199c323f6d5fb9c517bab04bea66a57161
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-06-13 02:03:15 +0530

    Added solution - LeetHub

--- Check if all levels of two trees are anagrams or not - GFG/check-if-all-levels-of-two-trees-are-anagrams-or-not.cpp
+++ Check if all levels of two trees are anagrams or not - GFG/check-if-all-levels-of-two-trees-are-anagrams-or-not.cpp
@@ -88,65 +88,52 @@ class Solution{
        q2.push(root2);
        int c=0;
        while(!q1.empty()){
-        //   c++;
-        //   cout<<c<<"c ";
-             //cout<<q1.size()<<"ss ";
+     
            if(q1.size()!=q2.size()) return false;
            vector<int> v1,v2;
            int l=q1.size();
            for(int i=0;i<l;i++)
            {
-                //cout<<q1.size()<<"ss ";
+                
                Node *temp1,*temp2,*t3,*t4;
                temp1=q1.front();
                temp2=q2.front();
                q1.pop();
                q2.pop();
-              // cout<<temp1->data<<" ";
-            //   t3=q1.front();
-            //   t4=q2.front();
+
                
                if(temp1->left) 
                {
-                  // cout<<temp1->left->data<<" ";
+                 
                    q1.push(temp1->left);
-                  // v1.push_back(temp1->left->data);
+                 
                }
                if(temp1->right) 
-               {  // cout<<temp1->right->data<<" ";
-                  // cout<<"yesl ";
+               {  
                    q1.push(temp1->right);
-                  // v1.push_back(temp1->right->data);
+                  
                }
                
                if(temp2->left) 
-               {  //  cout<<temp2->left->data<<" ";
+               { 
                    q2.push(temp2->left);
-                  // v2.push_back(temp2->left->data);
+               
                }
                if(temp2->right) 
-               { // cout<<temp2->right->data<<" ";
+               {
                    q2.push(temp2->right);
-                  // v2.push_back(temp2->right->data);
+                  
                }
-              // cout<<temp1->data<<t3->data<<"d"<<temp2->data<<t4->data<<" ";
-          
+             
                v1.push_back(temp1->data);
                v2.push_back(temp2->data);
                    
-               //cout<<endl;
-              // memcpy(&qu, &q1, sizeof(q1));
+               
               
            }
          
            
-        //   while(qu.empty()==false)
-        //   {
-        //       cout<<qu.front()<<" ";
-        //       qu.pop();
-        //   }
-           //cout<<endl;
-          // cout<<"end froyesIkn ";
+     
           sort(v1.begin(),v1.end());
           sort(v2.begin(),v2.end());
           if(v1!=v2) return false;
