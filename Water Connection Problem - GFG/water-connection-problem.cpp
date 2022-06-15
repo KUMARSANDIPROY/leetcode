27c267f128bbb4969ec8789584b460a27fc540e5
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-06-15 17:13:02 +0530

    Added solution - LeetHub

--- Water Connection Problem - GFG/water-connection-problem.cpp
+++ Water Connection Problem - GFG/water-connection-problem.cpp
@@ -6,15 +6,20 @@ using namespace std;
 class Solution
 {
     public:
-    void dfs(pair<int,int> adj[],int start,int &end,int &mn,vector<int> &vis,vector<int> out)
+    void dfs(vector<pair<int,int>> adj[],int start,int &end,int &mn,vector<int> &vis)
     {
         vis[start]=1;
-        if(out[start]==0) return;
-        end=adj[start].first;
-       // cout<<end<<"end  ";
-        mn=min(mn,adj[start].second);
-        if(vis[end]==0)
-           dfs(adj,adj[start].first,end,mn,vis,out);
+        for(auto i:adj[start])
+        {
+            if(vis[i.first]==0)
+            {
+                end=i.first;
+               // cout<<end<<"end  ";
+                mn=min(mn,i.second);
+                
+                dfs(adj,i.first,end,mn,vis);
+            }
+        }
     }
     
     vector<vector<int>> solve(int n,int p,vector<int> a,vector<int> b,vector<int> d)
@@ -24,14 +29,14 @@ class Solution
         vector<int> out(n+1,0);
         vector<int> vis(n+1,0);
        // vector<pair<int,int>> adj[n+1];
-        pair<int,int> adj[n+1];
+        vector<pair<int,int>> adj[n+1];
         vector<vector<int>> res;
         
         for(int i=0;i<p;i++)
         {
             out[a[i]]=1;
             in[b[i]]=1;
-            adj[a[i]]={make_pair(b[i],d[i])};
+            adj[a[i]].push_back({b[i],d[i]});
         }
         
         for(int i=1;i<=n;i++)
@@ -41,7 +46,7 @@ class Solution
                 int start=i;
                 int end;
                 int mn=INT_MAX;
-                dfs(adj,start,end,mn,vis,out);
+                dfs(adj,start,end,mn,vis);
                 
                 res.push_back({start,end,mn});
                // cout<<endl;
