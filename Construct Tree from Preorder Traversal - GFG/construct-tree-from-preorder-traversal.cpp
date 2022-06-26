3be2ad15f29e8a610d9a9fe56181f5f4ff233df5
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-06-26 14:51:01 +0530

    Added solution - LeetHub

---
+++ Construct Tree from Preorder Traversal - GFG/construct-tree-from-preorder-traversal.cpp
@@ -0,0 +1,90 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+void printInorder (struct Node* node)
+{
+	if (node == NULL)return;
+	printInorder(node->left);
+	printf("%d ", node->data);
+	printInorder (node->right);
+}
+
+Node* constructTree(int n, int pre[], char preLN[]);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* root = NULL;
+        int n;
+        cin>>n;
+        int pre[n];
+        char preLN[n];
+        for(int i=0; i<n; i++)cin>>pre[i];
+        for(int i=0; i<n; i++)cin>>preLN[i];
+        root = constructTree (n, pre, preLN);
+        printInorder(root);
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*Structure of the Node of the binary tree is as
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+*/
+// function should return the root of the new binary tree formed
+
+
+struct Node *solve(int n,int pre[], char preLN[],int &index1)
+{
+    if(index1==n) return NULL;
+    
+  
+    if(preLN[index1]=='L')
+    {
+        Node *temp=new Node(pre[index1++]);
+       
+        return temp;
+    }
+    else 
+    {
+        Node *temp=new Node(pre[index1++]);
+        temp->left=solve(n,pre,preLN,index1);
+        temp->right=solve(n,pre,preLN,index1);
+    
+  
+        return temp;  
+    }
+}
+
+struct Node *constructTree(int n, int pre[], char preLN[])
+{
+    // Code here
+    int index=0;
+    return solve(n,pre,preLN,index);
+}
