1f90a583df563b52cfa004a3d872ba3852d3f2aa
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-07-11 18:39:40 +0530

    Added solution - LeetHub

--- Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
+++ Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
@@ -8,37 +8,49 @@ class Solution
     public:
     //Function to find minimum number of attempts needed in 
     //order to find the critical floor.
-    /*  int static dp[201][201];
-            for(int i=0;i<201;i++) 
-            {
-                for(int j=0;j<201;j++)
-                  dp[i][j]=-1;
-            }*/
-   
-    int solve(int n,int k,vector<vector<int>> &dp)
-    {
-                if(dp[n][k]!=-1) return dp[n][k];
-                if(k==0 || k==1 ) return k;
-                
-                if(n==1) return k;
-           
-                
-                int mn=INT_MAX;
-                
-                for(int i=1;i<=k;i++)
-                {
-                    int temp=1+max(solve(n,k-i,dp),solve(n-1,i-1,dp));
-                    mn=min(mn,temp);
-                }
-                return dp[n][k]=mn;
-    }
     int eggDrop(int n, int k) 
     {
+        // your code here
+        int dp[n+1][k+1];
         
-                // your code here
-         vector<vector<int>> dp(209, vector<int> (209, -1));       
-           return solve(n,k,dp);     
-           
+        
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=k;j++)
+            {
+                if(j==0)
+                   dp[i][j]=0;
+                else if(j==1)
+                   dp[i][j]=1;
+                else if(i==1)
+                   dp[i][j]=j;
+                   
+                else if(i>1)
+                {
+                  
+                    int mn=INT_MAX;
+                    int mx;
+                    for(int mj=j-1,pj=0; mj>=0;mj--,pj++)
+                    {
+                        mx=max(dp[i-1][pj],dp[i][mj]);
+                        mn=min(mn,mx);
+                    }
+                    // cout<<i<<j<<mn<<"kj   ";
+                   dp[i][j]=1+mn; 
+                    
+                }
+            }
+        }
+        // cout<<endl;
+        //   for(int i=0;i<=n;i++)
+        // {
+        //     for(int j=0;j<=k;j++)
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[n][k];
     }
 };
 
