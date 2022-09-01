0f3138364d40958faa17492291ca8398c3249e8d
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-09-01 11:09:39 +0530

    Time: 387 ms (32.10%), Space: 123.3 MB (23.20%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,64 +1,33 @@
 class Solution {
 public:
-    int maxi=0;
-    
-    void solve(vector<string> &arr,string temp,int idx,map<char,int> mp)
-    {
-       if(idx==arr.size())
-       {
-           int sz=temp.size();
-           maxi=max(maxi,sz);
-           return ;
-       }
-       map<char,int>mp1, mp2;
-       mp2=mp;
-        string temp2=temp;
-        if(temp.size()==0)
-       {
-            int flag=0;
-            for(char c:arr[idx])
-            {    
-                mp1[c]++;
-                if(mp1[c]>1)
-                {
-                    flag=1;
-                    break;
-                }
-            }
-            if(flag==0)
-            solve(arr,arr[idx],idx+1,mp1);
-        }
-        if(temp.size()!=0)
-        {
-            int flag=0;
-            for(char c:arr[idx])
-            {
-               
-                if(mp.find(c)!=mp.end() || mp2.find(c)!=mp2.end())
-                {
-                    flag=1;
-                    break;
-                }
-                 mp2[c]++;
-            }
-            if(flag==0)
-            {
-                temp+=arr[idx];
-                solve(arr,temp,idx+1,mp2);
-            }
-
-        }
-        
-        
-        solve(arr,temp2,idx+1,mp);
-        
+    int maxi = 0;
+    int maxLength(vector<string> arr) {
+        dfs(0, arr, "");
+        return maxi;
     }
     
-    int maxLength(vector<string>& arr) {
-
-        string temp="";
-        map<char,int> mp;
-        solve(arr,temp,0,mp);
-        return maxi;
+     void dfs(int start, vector<string> arr, string str){
+        if(!isUnique(str)){
+            return;
+        }
+         int sz=str.size();
+        maxi = max(maxi, sz);
+        
+        for(int i=start; i<arr.size();i++){
+            string s = arr[i];
+            dfs(i+1, arr, str + s);
+        }
+    }
+    
+     bool isUnique(string s){
+        set<char> st;
+        
+        for(char ch: s){
+            if(st.find(ch)!=st.end()){
+                return false;
+            }
+            st.insert(ch);
+        }
+        return true;
     }
 };
