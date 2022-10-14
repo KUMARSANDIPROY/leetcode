ae0dced127532d2fa2f4889c7cf0b5f9ff28ba2a
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-10-14 10:58:02 +0530

    Time: 323 ms (48.41%), Space: 37.3 MB (35.40%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int knightDialer(int n) {
+        
+        vector<vector<int>> v={ {4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {4, 2}};
+        
+        int mod=1e9 +7;
+        vector<vector<int >> dp(n+1,vector<int>(10,0));
+        
+        for(int i=0;i<10;i++)
+        {
+            dp[1][i]=1;
+        }
+        
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=0;j<10;j++)
+            {
+                for(int k=0;k<v[j].size();k++)
+                {
+                    dp[i][j]=(dp[i][j]+dp[i-1][v[j][k]])%mod;
+                }
+                dp[i][j]=dp[i][j]%mod;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<10;i++)
+          sum=(sum+dp[n][i])%mod;
+        
+        return sum;                              
+                            
+    }
+};
