a24ea70ca941c9576e2592aa11df80e7c042f59b
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-10-15 02:33:03 +0530

    Added solution - LeetHub

--- Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
+++ Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
@@ -1,60 +1,40 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution
 {
     public:
     //Function to find minimum number of attempts needed in 
     //order to find the critical floor.
+   vector<vector<int>> dp;
+    
+    int solve(int n,int k)
+    {
+        if(n==1)  return k;
+        if(k==0) return 0;
+        
+        if(dp[n][k]!=-1)  return dp[n][k];
+        int ans=INT_MAX;
+        for(int i=1;i<=k;i++)
+        {
+            int temp=max(solve(n-1,i-1),solve(n,k-i));
+            ans=min(ans,1+temp);
+        }
+        return dp[n][k]=ans;
+    }
+    
     int eggDrop(int n, int k) 
     {
         // your code here
-        int dp[n+1][k+1];
-        
-        
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=k;j++)
-            {
-                if(j==0)
-                   dp[i][j]=0;
-                else if(j==1)
-                   dp[i][j]=1;
-                else if(i==1)
-                   dp[i][j]=j;
-                   
-                else if(i>1)
-                {
-                  
-                    int mn=INT_MAX;
-                    int mx;
-                    for(int mj=j-1,pj=0; mj>=0;mj--,pj++)
-                    {
-                        mx=max(dp[i-1][pj],dp[i][mj]);
-                        mn=min(mn,mx);
-                    }
-                    // cout<<i<<j<<mn<<"kj   ";
-                   dp[i][j]=1+mn; 
-                    
-                }
-            }
-        }
-        // cout<<endl;
-        //   for(int i=0;i<=n;i++)
-        // {
-        //     for(int j=0;j<=k;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        return dp[n][k];
+        dp.resize(201,vector<int>(201,-1));
+         return solve(n,k);
+       // return ans;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main()
 {
     //taking total testcases
@@ -71,4 +51,5 @@ int main()
     }
     return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
