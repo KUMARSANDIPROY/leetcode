cd2603ba1d22afed50ad9395dfb4fa1ece85315c
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-10-27 01:47:06 +0530

    Time: 22 ms (84.35%), Space: 6 MB (57.38%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -3,18 +3,18 @@ public:
     int count=0;
       void fun(vector<int>&nums,int index)
     {
-        if(index==nums.size())
+        if(index==0)
         {
             count+=1;
             return;
         }
         
         
-        for(int i=index;i<nums.size();i++)
+        for(int i=index;i>0;i--)
         {
             swap(nums[index],nums[i]);
-            if((nums[index]%(index+1)==0)||((index+1)%nums[index]==0))
-             fun(nums,index+1);
+            if((nums[index]%(index)==0)||((index)%nums[index]==0))
+             fun(nums,index-1);
             swap(nums[index],nums[i]);
         }
         
@@ -22,10 +22,10 @@ public:
     }
    int countArrangement(int n) {
         vector<int>nums;
-        for(int i=1;i<=n;i++)
+        for(int i=0;i<=n;i++)
             nums.push_back(i);
         count=0;
-        fun(nums,0);
+        fun(nums,n);
         return count;
     }
   
