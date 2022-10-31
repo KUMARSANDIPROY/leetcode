361290dbc3787fe08d04872bd8ab2682b635cb7a
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-10-31 12:17:35 +0530

    Added solution - LeetHub

--- Maximum Profit - GFG/maximum-profit.cpp
+++ Maximum Profit - GFG/maximum-profit.cpp
@@ -9,25 +9,25 @@ using namespace std;
 
 class Solution {
   public:
-    vector<vector<vector<int>>> dp;
-    int solve(int A[],int i,int trans,int N,int total,int buy)
+    vector<vector<int>> dp;
+    int solve(int A[],int i,int trans,int N,int total)
     {
         if(i==N || trans==total)  return 0;
         
         
-        if(dp[i][trans][buy]!=-1)  return dp[i][trans][buy];
-        if(buy==1)
-           return dp[i][trans][buy]= max(-A[i]+solve(A,i+1,trans+1,N,total,0), solve(A,i+1,trans,N,total,1) );
+        if(dp[i][trans]!=-1)  return dp[i][trans];
+        if(trans%2==0)
+           return dp[i][trans]= max(-A[i]+solve(A,i+1,trans+1,N,total), solve(A,i+1,trans,N,total) );
         else
-            return dp[i][trans][buy]=max(A[i]+solve(A,i+1,trans+1,N,total,1), solve(A,i+1,trans,N,total,0) );
+            return dp[i][trans]=max(A[i]+solve(A,i+1,trans+1,N,total), solve(A,i+1,trans,N,total) );
     }
   
     int maxProfit(int K, int N, int A[]) {
         // code here
         
         int total=2*K;
-        dp.resize(N+1,vector<vector<int>>(total+1,vector<int>(2,-1)));
-        return solve(A,0,0,N,total,1);
+        dp.resize(N+1,vector<int>(total+1,-1));
+        return solve(A,0,0,N,total);
     }
 };
 
