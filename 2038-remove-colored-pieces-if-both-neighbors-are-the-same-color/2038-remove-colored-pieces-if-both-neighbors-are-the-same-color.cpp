d214b8930a96d9c0d453271444718584763c660c
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-11-09 15:42:19 +0530

    Time: 37 ms (92.08%), Space: 13.3 MB (47.67%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,50 +1,19 @@
 class Solution {
 public:
     bool winnerOfGame(string colors) {
+     
+        int a=0,b=0;
         
-        int a=0;
-        int b=0;
-        int p1=0;
-        int p2=0;
-        int n=colors.length();
-        
-        char prev=colors[0];
-        if(prev=='A') a++;
-        else b++;
-        for(int i=1;i<n;i++)
+        for(int i=1;i<colors.size()-1;i++)
         {
-            if(colors[i]==prev)
+            if(colors[i-1]==colors[i] && colors[i]==colors[i+1])
             {
                 if(colors[i]=='A')
                     a++;
-                else
-                    b++;
+                else b++;
             }
-            else
-            {
-                if(prev=='A')
-                {
-                    p1+=max(0,(a-2));
-                    prev='B';
-                    a=0;
-                    b=1;
-                }
-                else
-                {
-                    p2+=max(0,(b-2));
-                    prev='A';
-                    a=1;
-                    b=0;
-                }
-                    
-            }
-            
         }
-        if(prev=='A') p1+=max(0,(a-2));
-        else p2+=max(0,(b-2));
         
-        cout<<p1<<p2<<"  ";
-        if(p1==p2 || p2>p1)  return false;
-        else  return true;
+        return a>b;
     }
 };
