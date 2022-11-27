2e0414533705e91e7fe07eaebf0986d1c0631348
KUMARSANDIPROY <52081779+KUMARSANDIPROY@users.noreply.github.com>
2022-11-27 17:00:17 +0530

    Time: 20 ms (37.57%), Space: 13.7 MB (64.48%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,53 +2,63 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         
+//         vector<int> v(2,-1);
+//         int n=nums.size();
+//         int l=0,r=n-1,mid;
+//          while(l<=r)
+//         {
+//              mid=(l+r)/2;
+//              //cout<<l<<r<<mid<<endl;
+//              if(nums[mid]==target)
+//                {  
+//                   // cout<<"Found";
+//                    break;
+//                }
+//                if(nums[mid]>target)
+//                {
+//                    r=mid-1;
+//                }
+//                else if(nums[mid]<target)
+//                {
+//                    l=mid+1;
+//                }
+//             }
+//         if(l>r) return v;
+//         else
+//         {
+            
+//             int i=mid,j=mid;
+//             if(mid!=0)
+//             {
+//                 while(nums[i-1]==target)
+//                 {
+//                     i--;
+//                     if(i==0) break;
+//                 }
+//             } 
+//             if(mid!=n-1)
+//             {
+//                 while(nums[j+1]==target)
+//                 {
+//                     j++;
+//                     if(j==n-1) break;
+//                 }
+//             }   
+//             v[0]=i;
+//             v[1]=j;
+            
+//                 return v;
+//         }
+        
         vector<int> v(2,-1);
-        int n=nums.size();
-        int l=0,r=n-1,mid;
-         while(l<=r)
-        {
-             mid=(l+r)/2;
-             //cout<<l<<r<<mid<<endl;
-             if(nums[mid]==target)
-               {  
-                  // cout<<"Found";
-                   break;
-               }
-               if(nums[mid]>target)
-               {
-                   r=mid-1;
-               }
-               else if(nums[mid]<target)
-               {
-                   l=mid+1;
-               }
-            }
-        if(l>r) return v;
-        else
-        {
-            
-            int i=mid,j=mid;
-            if(mid!=0)
-            {
-                while(nums[i-1]==target)
-                {
-                    i--;
-                    if(i==0) break;
-                }
-            } 
-            if(mid!=n-1)
-            {
-                while(nums[j+1]==target)
-                {
-                    j++;
-                    if(j==n-1) break;
-                }
-            }   
-            v[0]=i;
-            v[1]=j;
-            
-                return v;
-        }
+        if(lower_bound(nums.begin(),nums.end(),target)==nums.end()) 
+            return v;
+        int start=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        if(nums[start]!=target)  return v;
+        int up=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        v[0]=start;
+        v[1]=up-1;
+        return v;
         
     }
 };
